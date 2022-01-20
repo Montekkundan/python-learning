d06d43a27fe8d05ad2d2b7ae4e73231c43c8508f
Montek <montekkundan@gmail.com>
2022-01-20 18:49:35 +0530

    Calculator.py updated

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 import os
+
 logo = ''' 
  _____________________
 |  _________________  |                                     
@@ -19,5 +20,52 @@ logo = '''
 '''
 
 print(logo)
-os.system("pause")
 
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculate():
+    n1 = float(input("What's the first number: "))
+
+    for symbol in operations:
+        print(symbol)
+    user_continue = True
+    while user_continue:
+        user_choice = input("Pick an operation from above\n")
+        n2 = float(input("What's the next number: "))
+        calc = operations[user_choice]
+        answer = calc(n1, n2)
+        print(f"{n1} {user_choice} {n2} = {answer}")
+        user_choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation, or type end to stop: ")
+        if user_choice == 'y':
+            num1 = answer
+        elif user_choice == 'n':
+            user_continue = False
+            calculate()
+        elif user_choice == 'no':
+            user_continue = False
+
+
+calculate()
