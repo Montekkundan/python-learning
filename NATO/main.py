3882c0f8a89ce91cae7d3a68360f8dab205107a0
Montek <87750128+Montekkundan@users.noreply.github.com>
2022-03-13 13:59:12 +0530

    exception handled

--- NATO/main.py
+++ NATO/main.py
@@ -1,9 +1,18 @@
-
-
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 p_dic = {row.letter: row.code for (index, row) in data.iterrows()}
-word = input("Enter a word: \n").upper()
-output = [p_dic[letter] for letter in word]
-print(output)
+
+
+def generarte():
+    word = input("Enter a word: \n").upper()
+    try:
+        output = [p_dic[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letters in the alphabet please.")
+        generarte()
+    else:
+        print(output)
+
+
+generarte()
